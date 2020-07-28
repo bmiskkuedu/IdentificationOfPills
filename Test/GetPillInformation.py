c40163c50b09973cd2e10ecbaef57272ef027d20
bmiskkuedu <45959329+bmiskkuedu@users.noreply.github.com>
2020-07-28 17:58:32 +0900

    Update GetPillInformation.py
    Test시 blur 연산 적용 누락

--- Test/GetPillInformation.py
+++ Test/GetPillInformation.py
@@ -110,8 +110,11 @@ def GetImprintImg(img, className, initX, maxX, initY, maxY, ShowFlag = False):
 
     # 이미지 크기를 정해진 크기로 변경한다.
     croppingImg = cv.resize(croppingImg, (Configuration.ImageSize, Configuration.ImageSize))
+    
+    # blur 적용
+    croppingImg = cv.fastNlMeansDenoising(croppingImg, None, 50, 7, 15)
 
-        # black hat을 사용 할 경우
+    # black hat을 사용 할 경우
     kernel = np.ones((14, 14), np.uint8)
     blackhat = cv.morphologyEx(croppingImg, cv.MORPH_BLACKHAT, kernel)
 
